210d8750fffbca44b18f03e0317677d74f7cc67c
Kacper Chabros <chabroskacper@gmail.com>
2025-01-21 10:06:50 +0100

    modify start script

--- start_app.sh
+++ start_app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p ./app/data
-sudo chown www-data:docker ./app/data
+sudo chown 33:33 ./app/data
 sudo chmod 770 ./app/data
 
-docker-compose down --volumes --remove-orphans
-docker-compose up --build
+sudo docker-compose down --volumes --remove-orphans
+sudo docker-compose up --build
